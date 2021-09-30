6acf55940c0a759358194cbb73f2860b781c0ae6
Rokasbarasa1 <>
2021-09-30 20:00:36 +0200

    serial not working

--- auxiliary/src/lib.rs
+++ auxiliary/src/lib.rs
@@ -2,42 +2,62 @@
 
 #![no_std]
 
-#[allow(unused_extern_crates)] // NOTE(allow) rust-lang/rust#53964
+#[allow(unused_extern_crates)] // NOTE(allow) bug rust-lang/rust53964
 extern crate panic_itm; // panic handler
 
-pub use cortex_m::asm::{bkpt, nop};
+pub use cortex_m::{asm::bkpt, iprint, iprintln, peripheral::ITM};
 pub use cortex_m_rt::entry;
-pub use stm32f3::stm32f303::{rcc, tim6, RCC, TIM6};
-pub use stm32f3_discovery::switch_hal;
+pub use stm32f3_discovery::stm32f3xx_hal::pac::usart1;
 
-use stm32f3_discovery::{
-    leds::Leds,
-    stm32f3xx_hal::{prelude::*, stm32},
+pub mod monotimer;
+
+use stm32f3_discovery::stm32f3xx_hal::{
+    prelude::*,
+    serial::Serial,
+    pac::{self, USART1},
 };
+use monotimer::MonoTimer;
 
-pub fn init() -> (
-    Leds,
-    &'static rcc::RegisterBlock,
-    &'static tim6::RegisterBlock,
-) {
-    let p = stm32::Peripherals::take().unwrap();
+pub fn init() -> (&'static mut usart1::RegisterBlock, MonoTimer, ITM) {
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let dp = pac::Peripherals::take().unwrap();
 
-    let mut rcc = p.RCC.constrain();
+    let mut flash = dp.FLASH.constrain();
+    let mut rcc = dp.RCC.constrain();
 
-    let mut gpioe = p.GPIOE.split(&mut rcc.ahb);
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
 
-    let leds = Leds::new(
-        gpioe.pe8,
-        gpioe.pe9,
-        gpioe.pe10,
-        gpioe.pe11,
-        gpioe.pe12,
-        gpioe.pe13,
-        gpioe.pe14,
-        gpioe.pe15,
-        &mut gpioe.moder,
-        &mut gpioe.otyper,
-    );
+    let (tx, rx) = match () {
+        #[cfg(feature = "adapter")]
+        () => {
+            let mut gpioa = dp.GPIOA.split(&mut rcc.ahb);
 
-    (leds, unsafe { &*RCC::ptr() }, unsafe { &*TIM6::ptr() })
+            let tx = gpioa.pa9.into_af7_push_pull(&mut gpioa.moder, &mut gpioa.otyper, &mut gpioa.afrh);
+            let rx = gpioa.pa10.into_af7_push_pull(&mut gpioa.moder, &mut gpioa.otyper, &mut gpioa.afrh);
+
+            (tx, rx)
+        }
+        #[cfg(not(feature = "adapter"))]
+        () => {
+            let mut gpioc = dp.GPIOC.split(&mut rcc.ahb);
+
+            let tx = gpioc.pc4.into_af7_push_pull(&mut gpioc.moder, &mut gpioc.otyper, &mut gpioc.afrl);
+            let rx = gpioc.pc5.into_af7_push_pull(&mut gpioc.moder, &mut gpioc.otyper, &mut gpioc.afrl);
+
+            (tx, rx)
+        }
+    };
+
+    Serial::new(dp.USART1, (tx, rx), 115_200.Bd(), clocks, &mut rcc.apb2);
+    // If you are having trouble sending/receiving data to/from the
+    // HC-05 bluetooth module, try this configuration instead:
+    // Serial::usart1(dp.USART1, (tx, rx), 9600.bps(), clocks, &mut rcc.apb2);
+
+    unsafe {
+        (
+            &mut *(USART1::ptr() as *mut _),
+            MonoTimer::new(cp.DWT, clocks),
+            cp.ITM,
+        )
+    }
 }
